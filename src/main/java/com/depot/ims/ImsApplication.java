49456bea5275539c6d41db13d05b374d639ab84f
Cade Kenyon <cjkenyon@wisc.edu>
2024-03-06 22:59:44 -0600

    there is a site in the database base O_O

--- src/main/java/com/depot/ims/ImsApplication.java
+++ src/main/java/com/depot/ims/ImsApplication.java
@@ -1,13 +1,27 @@
 package com.depot.ims;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.depot.ims.models.Sites;
+import com.depot.ims.repositories.SitesRepository;
 
 @SpringBootApplication
 public class ImsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ImsApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner commandLineRunner(
+		SitesRepository sitesRepository
+	) {
+		return(args) -> {		
+			sitesRepository.save(new Sites(null, "HomeDepot 1", "W54 N54", "open", null, true));
+		};
 	}
 
 }
---
+++ src/main/java/com/depot/ims/repositories/SitesRepository.java
@@ -0,0 +1,9 @@
+package com.depot.ims.repositories;
+
+import com.depot.ims.models.Sites;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SitesRepository extends JpaRepository<Sites, Integer>{
+
+    
+}
