a019ab130cf4d5e2b8b8c6a27ecc1de2342578ec
Cade Kenyon <cjkenyon@wisc.edu>
2024-03-07 00:36:35 -0600

    adds sites by name

---
+++ src/main/java/com/depot/ims/controllers/SitesController.java
@@ -0,0 +1,46 @@
+package com.depot.ims.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.depot.ims.repositories.SitesRepository;
+
+import java.security.Timestamp;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.depot.ims.models.Sites;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping("/sites")
+public class SitesController {
+
+    private final SitesRepository sitesRepository;
+
+    public SitesController(SitesRepository sitesRepository) {
+        this.sitesRepository = sitesRepository;
+    }
+
+    @GetMapping
+    public List<Sites> getSites() {
+        return this.sitesRepository.findAll();
+    }
+
+    @GetMapping("/{siteName}")
+    public List<Sites> getSitesByName(@PathVariable String siteName) {
+        return this.sitesRepository.findBySiteName(siteName);
+    }
+
+    @PostMapping("/add")
+    public Sites postMethodName(@RequestBody String siteName) {
+
+        // return sitesRepository.save(new Sites(null, siteName, siteLocation, "open",
+        // null, true));
+        return sitesRepository.save(new Sites(null, siteName, "W45 N54", "open", null, true));
+    }
+
+}
