c68dfe7e305191ad52805f5fb30d7bb3e608c211
Legion <sli857@wisc.edu>
2024-03-11 21:05:17 -0500

    added items repo and controller

--- src/main/java/com/depot/ims/repositories/ItemsRepository.java
+++ src/main/java/com/depot/ims/repositories/ItemsRepository.java
@@ -1,4 +1,14 @@
 package com.depot.ims.repositories;
 
-public class ItemsRepository {
+import com.depot.ims.models.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface ItemsRepository extends JpaRepository<Item,Integer> {
+    List<Item> findAll();
+
+    @Query("SELECT i FROM Item i WHERE i.itemName = ?1")
+    List<Item> findItemsByItemName(String item_name);
 }
