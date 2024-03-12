3fb42654e5edd24690a52928a1e8cd80f5a07878
Lukasz Rzadkowski <rzadkowski@wisc.edu>
2024-03-11 19:49:26 -0500

    added ship repository interface

--- src/main/java/com/depot/ims/repositories/AvailabilityRepository.java
+++ src/main/java/com/depot/ims/repositories/AvailabilityRepository.java
@@ -16,4 +16,7 @@ public interface AvailabilityRepository extends JpaRepository<Availability, Inte
     @Query("select a from Availability a where a.itemId = ?1")
     List<Availability> findByItemId(Integer itemId);
 
+    @Query("select a from Availability a where a.siteId = ?1 and a.itemId = ?2")
+    Availability findBySiteIdAndItemId(Integer siteId, Integer itemId);
+
 }
---
+++ src/main/java/com/depot/ims/repositories/ShipRepository.java
@@ -0,0 +1,22 @@
+package com.depot.ims.repositories;
+
+import com.depot.ims.models.Ship;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface ShipRepository extends JpaRepository<Ship, Integer> {
+
+    List<Ship> findAll();
+
+    @Query("select s from Ship s where s.itemId = ?1")
+    List<Ship> findByItemId(Integer itemId);
+
+    @Query("select s from Ship s where s.shipmentId = ?1")
+    List<Ship> findByShipmentId(Integer shipmentId);
+
+    @Query("select s from Ship s where s.itemId = ?1 and s.shipmentId = ?2")
+    List<Ship> findByItemIdAndShipmentId(Integer itemId, Integer shipmentId);
+
+}
