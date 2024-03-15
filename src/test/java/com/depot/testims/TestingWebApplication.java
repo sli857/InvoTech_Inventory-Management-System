6639620798bf3ffb8acb349e5f82e13b46c10252
Cade Kenyon <cjkenyon@wisc.edu>
2024-03-14 19:11:54 -0500

    initial cicd

---
+++ src/test/java/com/depot/testims/TestingWebApplication.java
@@ -0,0 +1,12 @@
+package com.depot.testims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestingWebApplication.class, args);
+    }
+}
