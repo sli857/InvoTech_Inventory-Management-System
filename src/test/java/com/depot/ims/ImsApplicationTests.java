ec7278f5f93598d2b75d2f926b37529454ca137e
Tina Li <tli442@wisc.edu>
2024-04-01 23:49:57 -0500

    add item class unit test

--- src/test/java/com/depot/ims/ImsApplicationTests.java
+++ src/test/java/com/depot/ims/ImsApplicationTests.java
@@ -28,41 +28,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 class ImsApplicationTests {
-
-//	@Mock
-//	private SitesRepository sitesRepository;
-//	@InjectMocks
-//	private SitesController userController;
 
 	@Mock
 	private ItemsRepository itemsRepository;
 	@InjectMocks
 	private ItemsController itemsController;
 
-//	@Autowired
-//	private AvailabilitiesRepository availabilitiesRepository;
-//	@Autowired
-//	private SitesRepository sitesRepository;
-//	@Autowired
-//	private ItemsRepository itemsRepository;
-
 	@Autowired
 	private MockMvc mockMvc;
-
-	@Test
-	void contextLoads() {
-
-	}
-
-	@Test
-	public void testAvailability() {
-		//mock the repository class
-		//test the return of controller class
-
-	}
 
 	//use Junit test to test the controller class
 	@Test
@@ -70,58 +49,64 @@ class ImsApplicationTests {
 		//mock the repository class
 		//test the return of controller class
 
+		//test 1: test the getItem method inside the controller
 		//GET request
 		Item item = new Item(11L, "name", 1.1);
 		when(itemsRepository.findByItemId(11L)).thenReturn(item);
-		try {
-			mockMvc.perform(get("/items")
-							.contentType(MediaType.APPLICATION_JSON))
-					.andExpect(status().isOk()); // Expect HTTP status code 200 OK
-			//.andExpect(jsonPath("$.itemId").value("11L"));// Verify username
-//                    .andExpect(jsonPath("$.password").value("password")); // Verify password
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
+		//get the getItem method in the controller
+		ResponseEntity<?> response = this.itemsController.getItem(11L, "name");
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		assertNotNull(response.getBody());
+		assertEquals(true, response.getBody().equals(item));
+
+		//test 2: test the getItems method inside the item controller
+		Item item2 = new Item(12L, "name2", 1.2);
+		List<Item> list = new ArrayList<>();
+		list.add(item);
+		list.add(item2);
+		when(itemsRepository.findAll()).thenReturn(list);
+		ResponseEntity<?> response2 = this.itemsController.getItems();
+		assertEquals(HttpStatus.OK, response2.getStatusCode());
+		assertNotNull(response2.getBody());
+		assertEquals(true, response2.getBody().equals(list));
+
+		//test3.1: test the updateItem method inside the item controller with correct inputs
+		Item updateItem = new Item(11L, "newName", 2.1);
+		when(itemsRepository.existsById(11L)).thenReturn(true);
+		when(itemsRepository.save(updateItem)).thenReturn(updateItem);
+		ResponseEntity<?> response3 = this.itemsController.updateItem(11L, "newName", 2.1);
+		assertEquals(HttpStatus.OK, response3.getStatusCode());
+		assertNotNull(response3.getBody());
+		assertEquals(true, response3.getBody().equals(updateItem));
+
+		//test3.2: test the updateItem method inside the item with invalid inputs
+		ResponseEntity<?> response4 = this.itemsController.updateItem(11L, null, null);
+		assertEquals(HttpStatus.BAD_REQUEST, response4.getStatusCode());
+		assertNotNull(response4.getBody());
+		assertEquals( true, response4.getBody().equals("No value for this update is specified."));
+
+		System.out.println(response.getBody());
+		System.out.println(response2.getBody());
+		System.out.println(response3.getBody());
+
+		//test4.1: test the addItem method inside the item with valid item
+		Item newItem = new Item(20L, "new", 3.0);
+		ResponseEntity<?> response5= this.itemsController.addItem(newItem);
+		assertEquals(HttpStatus.OK, response5.getStatusCode());
+		assertNotNull(response5.getBody());
+		assertEquals( true, response5.getBody().equals(newItem));
 
 
 
-		//System.out.println(result.getResponse().getContentAsString());
-		System.out.println("hiii");
-//		ResponseEntity<?> response = this.itemsController.getItem(11L, "name");
-//		assertEquals(HttpStatus.OK, response.getStatusCode());
-//		assertNotNull(response.getBody());
-//		assertEquals(true, response.getBody().equals(item));
+	}
 
 
-
-//		assertEquals("name", response.getBody().getUsername());
-//		assertEquals("password", response.getBody().getPassword());
-		//fail();
-
-		//when(sitesRepository.)
+	@Test
+	public void testAvailability() {
+		//mock the repository class
+		//test the return of controller class
 
 
-		//testing repository
-//		Site site1 = new Site(1L, "HomeDepot 2", "W54 N53", "open", null, true);
-//		Site site2 = new Site(2L, "HomeDepot 3", "W54 N52", "open", null, true);
-//		sitesRepository.save(site1);
-//		sitesRepository.save(site2);
-//		sitesRepository.updateSiteName(2L, "newname");
-//		Site actualSite = sitesRepository.findBySiteId(2L);
-//		String name = actualSite.getSiteName();
-//		assertEquals(name, "newname");
-
-
-//		sitesRepository.save(new Site(3L, "HomeDepot 4", "W54 N51", "open", null, true));
-//////            sitesRepository.save(new Site(null, "HomeDepot 5", "W54 N50", "open", null, true));
-//		Item item1 = new Item(1L, "item1", 2.0);
-//		Item item2 = new Item(2L, "item2", 3.0);
-//		itemsRepository.save(item1);
-//		itemsRepository.save(item2);
-//		itemsRepository.save(new Item(3L, "item3", 4.0));
-//
-//		availabilitiesRepository.save(new Availability(site1, item1, 10));
-//		availabilitiesRepository.save(new Availability(site2, item1, 20));
 
 	}
 
