adef36be85a7d16d00fc45d1fcad0ce77a718892
MIHAILO POPOVIC <mpopovic3@wisc.edu>
2024-04-02 19:58:07 -0500

    Support for filtering the sites based on all attributes

--- web/src/components/structural/IMSApp.jsx
+++ web/src/components/structural/IMSApp.jsx
@@ -2,8 +2,7 @@ import {Route, Routes } from "react-router-dom";
 
 // Import layout and page components
 import IMSLayout from "./IMSLayout";
-import IMSHome from "./../content/IMSHome"
-import IMSWarehouses from "./../content/IMSWarehouses"
+import IMSSites from "./../content/IMSSites"
 import IMSShipments from "./../content/IMSShipments"
 import IMSNoMatch from "./../content/IMSNoMatch"
 import IMSSite from "../content/IMSSite";
@@ -15,10 +14,9 @@ import IMSSite from "../content/IMSSite";
  * <IMSLayout> is used as a wrapper for the main content routes, providing consistent layout across pages.
  * 
  * Routes:
- * - "/" : Renders the IMSHome component as the homepage.
+ * - "/" : Renders the IMSSites component as the homepage.
  * - "/site/:siteId" : Dynamically renders details for a specific site using the IMSSite component.
  * - "/shipments" : Renders the IMSShipments component to show shipment information.
- * - "/warehouses" : Renders the IMSWarehouses component to show warehouse information. THIS PAGE WILL NOT BE USED
  * - "*" : Catch-all route that renders the IMSNoMatch component for unmatched paths (404).
  * 
  * @returns {JSX.Element} The router setup for the application.
@@ -27,10 +25,9 @@ function IMSApp() {
   return (
     <Routes>
       <Route path="/" element={<IMSLayout />}>
-        <Route index element={<IMSHome />} />
+        <Route index element={<IMSSites />} />
         <Route path="/site/:siteId" element={<IMSSite />} />
         <Route path="shipments" element={<IMSShipments />} />
-        <Route path="warehouses" element={<IMSWarehouses />} />
         <Route path="*" element={<IMSNoMatch />} />
       </Route>
     </Routes>
--- web/src/components/structural/IMSLayout.jsx
+++ web/src/components/structural/IMSLayout.jsx
@@ -30,13 +30,10 @@ function IMSLayout() {
           </Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link as={Link} to="/">
-              Home
+              Sites
             </Nav.Link>
             <Nav.Link as={Link} to="shipments">
               Shipments
-            </Nav.Link>
-            <Nav.Link as={Link} to="warehouses">
-              Warehouse
             </Nav.Link>
           </Nav>
         </Container>
