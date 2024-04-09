7d2292df73d8b8628b50266b19eb053717e428d3
Tina Li <tli442@wisc.edu>
2024-04-08 19:52:04 -0500

    test

--- src/test/java/com/depot/ims/repositories/ItemTest.java
+++ src/test/java/com/depot/ims/repositories/ItemTest.java
@@ -1,27 +1,27 @@
-//package com.depot.ims.repositories;
-//
-//import com.depot.ims.models.Site;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import static org.junit.jupiter.api.Assertions.*;
-//import com.depot.ims.models.Item;
-//import org.springframework.test.context.TestPropertySource;
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//@DataJpaTest
-//// Annotation to specify property sources for the test
-//@TestPropertySource(properties = {
-//        "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
-//        "spring.jpa.hibernate.ddl-auto=update"
-//})
-//public class ItemTest {
-//
-//    @Autowired
-//    private ItemRepository itemRepository;
-//
+package com.depot.ims.repositories;
+
+import com.depot.ims.models.Site;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import static org.junit.jupiter.api.Assertions.*;
+import com.depot.ims.models.Item;
+import org.springframework.test.context.TestPropertySource;
+import java.util.ArrayList;
+import java.util.List;
+
+@DataJpaTest
+// Annotation to specify property sources for the test
+@TestPropertySource(properties = {
+        "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+        "spring.jpa.hibernate.ddl-auto=update"
+})
+public class ItemTest {
+
+    @Autowired
+    private ItemRepository itemRepository;
+
 //    @BeforeEach
 //    void setup() {
 //        Item item1 = createItem("item 1", 12.99);
@@ -35,7 +35,7 @@
 ////        assertNotNull(list);
 ////        assertEquals(2, list.size());
 //    }
-//
+
 //    @Test
 //    void TestFindByItemId() {
 //        System.out.println(this.itemRepository.findAll());
@@ -43,17 +43,17 @@
 ////        assertNotNull(item);
 //
 //    }
-//
-//    private Item createItem( String name, double price) {
-//        Item item = Item.builder()
-//                .itemName(name)
-//                .itemPrice(price)
-//                .build();
-//        return itemRepository.saveAndFlush(item);
-//    }
-//
-//
-//
-//
-//
-//}
+
+    private Item createItem( String name, double price) {
+        Item item = Item.builder()
+                .itemName(name)
+                .itemPrice(price)
+                .build();
+        return itemRepository.saveAndFlush(item);
+    }
+
+
+
+
+
+}
